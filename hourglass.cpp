6424361dd70cae019f00e1726209459433a29975
ruriro0125 <ruriro0125@gmail.com>
2018-07-05 23:41:57 +0900

    砂時計を描画するプログラム

---
+++ hourglass.cpp
@@ -0,0 +1,63 @@
+//こちらのプログラムは砂時計を出力するプログラムです
+
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+
+bool checkInput(std::string input);
+void display(int mode);
+
+int start();
+void display(int mode);
+
+int main() {
+    int mode;
+    std::string check;
+    std::cout << "砂時計を表示します" << "\n" << "何段表示するか入力してください：";
+    std::cin >> check;
+    std::cout << "\n";
+    //入力されたものが数字か文字か判定
+    if(!checkInput(check)) {
+        std::cout << "error" << std::endl;
+        return 1;
+    }
+    mode = std::stoi(check);
+    //砂時計を出力する
+    display(mode);
+}
+
+//文字が入っていた場合falseを出力する
+bool checkInput(std::string input) {
+    return std::all_of(input.cbegin(),input.cend(),isdigit);
+}
+
+//砂時計を出力する
+void display(int mode) {
+    std::string space = " ";
+    //iは何段目を指している
+    for(int i = mode;i > 1;i--) {
+        for(int j = 0;j < mode - i;j++) {
+            std::cout << space;
+        }
+        for(int j = 0;j < i;j++) {
+            std::cout << " " << "*";
+        }
+        for(int j = 0;j < mode - i;j++) {
+            std::cout << space;
+        }
+        std::cout << std::endl;
+    }
+    for(int i = 1;i < mode+1;i++) {
+        //i段目の出力
+        for(int j = 0;j < mode -i;j++) {
+            std::cout << space;
+        }
+        for(int j = 0;j < i;j++) {
+            std::cout << " " << "*";
+        }
+        for(int j = 0;j < mode;j++) {
+            std::cout << space;
+        }
+        std::cout << std::endl;
+    }
+}
