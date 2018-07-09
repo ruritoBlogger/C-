df4948c675098cfe9329ddaf82f73cdf7174a8ff
ruriro0125 <ruriro0125@gmail.com>
2018-07-09 09:42:07 +0900

    入力された配列の中から指定された範囲の和を出力する

---
+++ array6.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> array;
+
+int main() {
+    std::cout << "入力された配列の中から指定された範囲の和を出力する" << std::endl;
+    
+    //配列数の入力
+    std::cout << "配列数の入力：" << std::endl;
+    int length;
+    std::cin >> length;
+    std::cout << std::endl;
+
+    //配列の入力
+    array = std::vector<int>(length);
+    for(int line = 0;line < length;line++) {
+        std::cout << line+1 << "番目の配列：";
+        std::cin >> array[line];
+        std::cout << std::endl;
+    }
+
+    //範囲の入力及び結果の出力
+    while(true) {
+        //範囲の指定
+        std::cout << "出力したい和の範囲の指定" << std::endl;
+        std::cout << "ひとつ目：";
+        int point1 = 0;
+        int point2 = 0;
+        std::cin >> point1;
+        std::cout << "ふたつ目：";
+        std::cin >> point2;
+        std::cout << std::endl;
+
+        //結果の出力
+        int answer = 0;
+        for(int line = (point1-1);line < point2;line++) {
+            answer += array[line];
+        }
+        std::cout << answer << std::endl;
+    }
+}
